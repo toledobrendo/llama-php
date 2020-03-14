01ba9aacbbaf4f343bad8da4dc071e20229aedd9
Ke'an Consolacion <keanconsolacion@gmail.com>
2020-03-14 14:52:05 +0800

    Seatwork 1

---
+++ bobs-auto-parts/process-order.php
@@ -0,0 +1,43 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf+8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  </head>
+  <body>
+    <!--Can be seen in browser-->
+    <div class="container">
+      <div class="card">
+        <div class="card-body">
+          <h3 class="cart-title">Order Results</h3>
+
+          <?php
+          echo '<p> Order Processed at ';
+          echo date('H:i, jS F Y');
+          echo '</p>';
+
+          // PHP Comments
+
+          $tireQty = $_POST['tireQty'];
+          $oilQty = $_POST['oilQty'];
+          $sparkQty = $_POST['sparkQty'];
+
+          echo '<p>Your order is as follows</p>';
+          echo $tireQty.' Tire<br/>';
+          echo $oilQty.' Oil<br/>';
+          echo $sparkQty.' Spark Plugs<br/>';
+          
+           ?>
+        </div>
+      </div>
+    </div>
+
+     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+
+</html>
