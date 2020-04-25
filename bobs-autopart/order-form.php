507e94875269d59cdcd100d654a80ee368d7883e
nimbly-dev <nimbly2016@gmail.com>
2020-04-25 17:08:38 +0800

    Bob

--- bobs-autopart/order-form.php
+++ bobs-autopart/order-form.php
@@ -1,70 +1,77 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous">
   </head>
   <body>
+    <!-- hello-world.php or hello_world.php -->
     <div class="container">
-      <div class="card">
+      <div class="card mt-3">
         <div class="card-body">
-          <h3 class="card-title">Orderform</h3>
+          <h3 class="card-title">Order Form</h3>
           <form action="process-order.php" method="post">
-            <table class = "table">
+            <table class="table">
               <thead>
                 <tr class="row">
-                  <th class="col-5">Items</th>
+                  <th class="col-5">Item</th>
                   <th class="col-4">Quantity</th>
                 </tr>
               </thead>
               <tbody>
+                <?php
+
+                $productInputs = array(
+                    array('Product' => 'Tire', 'name' => 'tireQty'),
+                    array('Product' => 'Oil', 'name' => 'oilQty'),
+                    array('Product' => 'Spark Plug', 'name' => 'sparkQty')
+                  );
+
+                foreach($productInputs as $productInput){
+                  echo '<tr class="row">
+                          <td class="col-5">'.$productInput['Product'].'</td>
+                          <td class="col-6">
+                            <input type="number" name="'.$productInput['name'].'"  min="0" class="form-control"/>
+                          </td>
+                        </tr>';
+                }
+
+                ?>
+
                 <tr class="row">
-                  <td class="col-5"> Tires</td>
+                  <td class="col-5">How did you find Bob's</td>
                   <td class="col-4">
-                    <input type="text" name="tireQty" maxlength="3" min="0" max="10" class="form-control">
+                    <select name="find" class="custom-select">
+                      <option value="regular">I'm a regular customer</option>
+                      <option value="tv">Television advertising</option>
+                      <option value="phone">Phone</option>
+                      <option value="mouth">A Person</option>
+                    </select>
                   </td>
-                </tr>
-                <tr class="row">
-                  <td class="col-5"> Oil</td>
-                  <td class="col-4">
-                    <input type="text" name="oilQty" maxlength="3" min="0" max="10" class="form-control">
-                  </td>
-                </tr>
-				        <tr class="row">
-                  <td class="col-5"> Spark Plugs </td>
-                  <td class="col-4">
-                    <input type="text" name="sparkQty" maxlength="3" min="0" max="10" class="form-control">
-                  </td>
-                </tr>
-                <tr class="row">
-                    <td class="col-5">How did you find Bob's?</td>
-                    <td class="col-4">
-                      <select name="find" class="custom-select">
-                        <option value="regular">I'm a regular customer</option>
-                        <option value="tv">TV Advertising</option>
-                        <option value="phone">Phone's Direction</option>
-                        <option value="mouth">TV Advertising</option>
-                    </td>
                 </tr>
                 <tr class="row">
                   <td colspan="2" class="col-9">
-                    <a href="freight-cost.php" class="btn btn-secondary">Freight Cost</a>
-                    <button type="submit" class="btn btn-primary float-right">Submit</button>
+                    <button type="submit" class="btn btn-success float-right ">Submit</button>
+                    <a href="freight-cost.php" class="btn btn-warning float-right mr-3">Freight</a>
                   </td>
-
                 </tr>
-
               </tbody>
             </table>
           </form>
         </div>
       </div>
     </div>
-
-     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+      crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+      crossorigin="anonymous"></script>
   </body>
-
 </html>
