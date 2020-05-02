4d4ef04f2f1ef0172f60e5da19548eb0cc580b46
JeffCantores <jeffersoncantores13@gmail.com>
2020-05-02 15:50:49 +0800

    Added header and footer files

--- bobs-auto-parts/order-form.php
+++ bobs-auto-parts/order-form.php
@@ -1,18 +1,7 @@
+<?php
+  require_once('view-comp/header.php');
+ ?>
 
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-      crossorigin="anonymous">
-  </head>
-  <body>
-    <!-- hello-world.php or hello_world.php -->
-    <div class="container">
-      <div class="card">
-        <div class="card-body">
           <h3 class="card-title">Order Form</h3>
           <form action="process-order.php" method="post">
                 <?php
