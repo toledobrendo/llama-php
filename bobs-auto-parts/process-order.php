3e2360888ef6c625a4c3741d03f8ddb626e4e364
paolotormon <mdawntormon@gmail.com>
2020-05-16 00:31:50 +0800

    fixed indentation and replied to note

--- bobs-auto-parts/process-order.php
+++ bobs-auto-parts/process-order.php
@@ -1,119 +1,124 @@
 <!-- Note: Indention on PHP scripts -->
-<?php
-require_once("view-comp/header.php");
 
-require_once('product-factory.php');
-require_once('service/order-service.php');
-define("VAT_PERCENT",getVAT());// <-HOMEWORK 4
+<!-- Reply: Hi sir, my formatter auto-indents it that way po, I think
+it's following something like here: https://gist.github.com/ryansechrest/8138375
+where there is no indentation after the opening php tag and the last php
+closing tag is truncated-->
+<?php
+   require_once("view-comp/header.php");
+
+   require_once('product-factory.php');
+   require_once('service/order-service.php');
+   define("VAT_PERCENT",getVAT());// <-HOMEWORK 4
 
 ?>
 <h3 class="card-title">Order Result</h3>
 <?php
-echo '<p>Order Processed at ';
-echo date('H:i, jS F Y');
-echo '</p>';
+   echo '<p>Order Processed at ';
+   echo date('H:i, jS F Y');
+   echo '</p>';
 
-// PHP Comments
-/**Multiline Comments
-              Wow**/
+   // PHP Comments
+   /**Multiline Comments
+                Wow**/
 
-//"@" for supress warnings
-@$tireQty = $_POST['tireQty'] ? $_POST['tireQty'] : 0;
-@$oilQty = $_POST['oilQty'] ? $_POST['oilQty'] : 0;
-@$sparkQty = $_POST['sparkQty'] ? $_POST['sparkQty'] : 0;
-@$find = $_POST['find'];
+   //"@" for supress warnings
+   @$tireQty = $_POST['tireQty'] ? $_POST['tireQty'] : 0;
+   @$oilQty = $_POST['oilQty'] ? $_POST['oilQty'] : 0;
+   @$sparkQty = $_POST['sparkQty'] ? $_POST['sparkQty'] : 0;
+   @$find = $_POST['find'];
 
-switch ($find) {
-   case 'regular':
-      echo 'Regular Customer';
-      break;
-   case 'tv':
-      echo 'From TV Advertising';
-      break;
-   case 'phone':
-      echo 'From Phone Directory';
-      break;
-   case 'mouth':
-      echo 'From Word of Mouth';
-      break;
-   default:
-      echo 'Unknown Customer';
-      break;
-}
+   switch ($find) {
+      case 'regular':
+         echo 'Regular Customer';
+         break;
+      case 'tv':
+         echo 'From TV Advertising';
+         break;
+      case 'phone':
+         echo 'From Phone Directory';
+         break;
+      case 'mouth':
+         echo 'From Word of Mouth';
+         break;
+      default:
+         echo 'Unknown Customer';
+         break;
+   }
 
-echo '<br/><br/>';
-echo '<p>Prices<br/>';
-echo 'Tires: ' . $tires->price . '<br/>';
-echo 'Oil: ' . $oil->price . '<br/>';
-echo 'Spark Plugs: ' . $sparkPlug->price . '<br/><br/>';
+   echo '<br/><br/>';
+   echo '<p>Prices<br/>';
+   echo 'Tires: ' . $tires->price . '<br/>';
+   echo 'Oil: ' . $oil->price . '<br/>';
+   echo 'Spark Plugs: ' . $sparkPlug->price . '<br/><br/>';
 
-$totalQty = @($tireQty + $oilQty + $sparkQty);
+   $totalQty = @($tireQty + $oilQty + $sparkQty);
 
-if ($totalQty == 0) {
-   echo 'You didn\'t order anything. <br/> <br/>';
-} else {
-   echo '<p>Your order is as follows</p>';
-   // echo $tireQty.' $tireQty tires<br/>';
-   if ($tireQty > 0)
-      echo "$tireQty tires<br/>";
-   if ($oilQty > 0)
-      echo $oilQty . ' oil<br/>';
-   if ($sparkQty > 0)
-      echo $sparkQty . ' spark plugs<br/>';
-   echo '<br/>';
-}
-echo 'Total Quantity: ' . $totalQty . '<br/>';
+   if ($totalQty == 0) {
+      echo 'You didn\'t order anything. <br/> <br/>';
+   } else {
+      echo '<p>Your order is as follows</p>';
+      // echo $tireQty.' $tireQty tires<br/>';
+      if ($tireQty > 0)
+         echo "$tireQty tires<br/>";
+      if ($oilQty > 0)
+         echo $oilQty . ' oil<br/>';
+      if ($sparkQty > 0)
+         echo $sparkQty . ' spark plugs<br/>';
+      echo '<br/>';
+   }
+   echo 'Total Quantity: ' . $totalQty . '<br/>';
 
-$tireAmount = @($tireQty * $tires->price);
-$oilAmount = @($oilQty * $oil->price);
-$sparkAmount = @($sparkQty * $sparkPlug->price);
+   $tireAmount = @($tireQty * $tires->price);
+   $oilAmount = @($oilQty * $oil->price);
+   $sparkAmount = @($sparkQty * $sparkPlug->price);
 
-$totalAmount = (float) $tireAmount;
+   $totalAmount = (float) $tireAmount;
 
-$otherTotalAmount = &$totalAmount;
-$otherTotalAmount += $oilAmount;
-$totalAmount += $sparkAmount;
+   $otherTotalAmount = &$totalAmount;
+   $otherTotalAmount += $oilAmount;
+   $totalAmount += $sparkAmount;
 
-echo 'Other Total Amount: ' . $otherTotalAmount . '<br/>';
-echo 'Total Amount: ' . $totalAmount . '<br/>';
+   echo 'Other Total Amount: ' . $otherTotalAmount . '<br/>';
+   echo 'Total Amount: ' . $totalAmount . '<br/>';
 
 
-/* if
- *    $totalAmount = $vatableAmount + $vat
- *    $vat = VAT_PERCENT * $vatableAmount
- *
- * then it means
- *
- *    $totalAmount = $vatableAmount + (VAT_PERCENT * $vatableAmount)
- *    $totalAmount = $vatableAmount * (VAT_PERCENT + 1)
- *
- * divide both sides by (VAT_PERCENT + 1):
- *
- *    $totalAmount / (VAT_PERCENT + 1) = $vatableAmount
- *    $vatableAmount = $totalAmount / (VAT_PERCENT + 1);         */
+   /* if
+   *    $totalAmount = $vatableAmount + $vat
+   *    $vat = VAT_PERCENT * $vatableAmount
+   *
+   * then it means
+   *
+   *    $totalAmount = $vatableAmount + (VAT_PERCENT * $vatableAmount)
+   *    $totalAmount = $vatableAmount * (VAT_PERCENT + 1)
+   *
+   * divide both sides by (VAT_PERCENT + 1):
+   *
+   *    $totalAmount / (VAT_PERCENT + 1) = $vatableAmount
+   *    $vatableAmount = $totalAmount / (VAT_PERCENT + 1);         */
 
-$vatableAmount = $totalAmount / (VAT_PERCENT + 1);
+   $vatableAmount = $totalAmount / (VAT_PERCENT + 1);
 
-// $vatableAmount = $totalAmount / (1.12)
+   // $vatableAmount = $totalAmount / (1.12)
 
-$vat = $totalAmount - $vatableAmount;
+   $vat = $totalAmount - $vatableAmount;
 
-echo 'VATable Amount: ' . $vatableAmount . '<br/>';
-echo 'VAT: ' . $vat . '<br/>';
+   echo 'VATable Amount: ' . $vatableAmount . '<br/>';
+   echo 'VAT: ' . $vat . '<br/>';
 
-echo 'Amount exceeded 500? ' . ($totalAmount > 500 ? 'Yes' : 'No') . '<br/><br/>';
+   echo 'Amount exceeded 500? ' . ($totalAmount > 500 ? 'Yes' : 'No') . '<br/><br/>';
 
-echo 'Is $totalAmount string? ' . (is_string($totalAmount) ? 'Yes' : 'No') . '<br/>';
+   echo 'Is $totalAmount string? ' . (is_string($totalAmount) ? 'Yes' : 'No') . '<br/>';
 
-// unset($totalAmount);
+   // unset($totalAmount);
 
-echo 'Is $totalAmount set? ' . (isset($totalAmount) ? 'Yes' : 'No') . '<br/>';
+   echo 'Is $totalAmount set? ' . (isset($totalAmount) ? 'Yes' : 'No') . '<br/>';
 
-$totalAmountTwo = 0;
-echo 'Is $totalAmountTwo set? ' . (isset($totalAmountTwo) ? 'Yes' : 'No') . '<br/>';
-echo 'Is $totalAmountTwo empty? ' . (empty($totalAmountTwo) ? 'Yes' : 'No') . '<br/>';
+   $totalAmountTwo = 0;
+   echo 'Is $totalAmountTwo set? ' . (isset($totalAmountTwo) ? 'Yes' : 'No') . '<br/>';
+   echo 'Is $totalAmountTwo empty? ' . (empty($totalAmountTwo) ? 'Yes' : 'No') . '<br/>';
 
-@saveOrder($tireQty, $oilQty, $sparkQty, $totalAmount);
+   @saveOrder($tireQty, $oilQty, $sparkQty, $totalAmount);
 ?>
 
 
@@ -121,5 +126,5 @@ echo 'Is $totalAmountTwo empty? ' . (empty($totalAmountTwo) ? 'Yes' : 'No') . '<
 <div class="card-footer">
    <a class="btn btn-info" href="order-form.php">Go Back</a>
    <?php
-   include("view-comp/footer.php")
+      include("view-comp/footer.php")
    ?>
