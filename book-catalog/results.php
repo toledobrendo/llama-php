38ff5a80492aa4310451acc52c2ca4cb97803110
cams_p <cnpascua00@gmail.com>
2020-05-16 16:04:24 +0800

    added db

---
+++ book-catalog/results.php
@@ -0,0 +1,13 @@
+<?php require_once('view-comp/header.php') ?>
+<div class="card-header">
+  Book Results
+</div>
+
+<div class="card-body">
+  <?php
+    $searchType = $_POST['searchType'];
+    $searchTerm = $_POST['searchTerm'];
+
+   ?>
+</div>
+<?php require_once('view-comp/footer.php') ?>
