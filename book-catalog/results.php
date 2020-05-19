66d3d037b0a9f57f12bc97f6e7207cb0562476ed
paolotormon <mdawntormon@gmail.com>
2020-05-19 23:56:48 +0800

    fixed h5 indentation

--- book-catalog/results.php
+++ book-catalog/results.php
@@ -4,45 +4,45 @@
 </div>
 <div class="card-body">
    <?php
-   define('FIELDS', array(
-      'author' => 'author.name',
-      'title' => 'book.title',
-      'isbn' => 'book.isbn'
-   ));
+      define('FIELDS', array(
+         'author' => 'author.name',
+         'title' => 'book.title',
+         'isbn' => 'book.isbn'
+      ));
 
-   $searchType = $_POST['searchType'];
-   $searchTerm = $_POST['searchTerm'];
+      $searchType = $_POST['searchType'];
+      $searchTerm = $_POST['searchTerm'];
 
-   try {
-      if (!$searchType || !$searchTerm) {
-         throw new Exception('You have not entered search details. Please go back and try again.', 1);
-      }
+      try {
+         if (!$searchType || !$searchTerm) {
+            throw new Exception('You have not entered search details. Please go back and try again.', 1);
+         }
 
-      // 127.0.0.1 = localhost
-      @$db = new mysqli('127.0.0.1:3306', 'student', 'qwe123', 'php_lesson_db');
+         // 127.0.0.1 = localhost
+         @$db = new mysqli('127.0.0.1:3306', 'student', 'qwe123', 'php_lesson_db');
 
-      $dbError = mysqli_connect_errno();
-      if ($dbError) {
-         throw new Exception('Error: Could not connect to database. ' .
-            'Please try again later. ' . $dbError, 1);
-      }
+         $dbError = mysqli_connect_errno();
+         if ($dbError) {
+            throw new Exception('Error: Could not connect to database. ' .
+               'Please try again later. ' . $dbError, 1);
+         }
 
-      $query = 'SELECT author.name AS author_name, book.title, book.isbn, book.image_location
-        FROM book
-        INNER JOIN author
-            ON author.id = book.author_id
-        WHERE ' . FIELDS[$searchType] . ' LIKE \'%' . $searchTerm . '%\';';
+         $query = 'SELECT author.name AS author_name, book.title, book.isbn, book.image_location
+         FROM book
+         INNER JOIN author
+               ON author.id = book.author_id
+         WHERE ' . FIELDS[$searchType] . ' LIKE \'%' . $searchTerm . '%\';';
 
-      $result = $db->query($query);
-      
-      $resultCount = $result->num_rows;
+         $result = $db->query($query);
 
-      echo '<p>Result for ' . $searchType . ' : ' . $searchTerm . '</br>';
-      echo 'Number of books found: ' . $resultCount;
+         $resultCount = $result->num_rows;
 
-      echo '<div class="row">';
-      for ($ctr = 0; $ctr < $resultCount; $ctr++) {
-         $row = $result->fetch_array();
+         echo '<p>Result for ' . $searchType . ' : ' . $searchTerm . '</br>';
+         echo 'Number of books found: ' . $resultCount;
+
+         echo '<div class="row">';
+         for ($ctr = 0; $ctr < $resultCount; $ctr++) {
+            $row = $result->fetch_array();
    ?>
          <div class="card col-4 mx-1">
             <div class="row card-body">
@@ -59,12 +59,12 @@
             </div>
          </div>
    <?php
+         }
+         echo '</div>';
+         echo '<p>Query done: ' . $query . '<br/></p>';
+      } catch (Exception $e) {
+         echo $e->getMessage();
       }
-      echo '</div>';
-      echo '<p>Query done: ' . $query . '<br/></p>';
-   } catch (Exception $e) {
-      echo $e->getMessage();
-   }
    ?>
    <br />
    <a class="btn btn-secondary" href="index.php">Go Back</a>
