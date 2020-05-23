f292e56d587bed8b50dac6d5d914e8016e0ffd25
JeffCantores <jeffersoncantores13@gmail.com>
2020-05-23 16:17:41 +0800

    Added "Add author" feature

---
+++ book-catalog/author-add.php
@@ -0,0 +1,18 @@
+<?php require_once('view-comp/header.php'); ?>
+
+<div class="card-header">
+  Add Author
+</div>
+<div class="card-body">
+    <form class="" action="process-author-add.php" method="post">
+      <div class="form-group">
+          <label for="authorName">Author Name</label>
+          <input class="form-control" type="text" id ="authorName" name="authorName"/>
+      </div>
+      <div>
+        <button class="btn btn-dark" type="submit">Submit</button>
+      </div>
+    </form>
+</div>
+
+<?php require_once('view-comp/footer.php'); ?>
---
+++ book-catalog/process-author-add.php
@@ -0,0 +1,60 @@
+<?php require_once('view-comp/header.php'); ?>
+
+<div class="card-header">
+  Add Author Result
+</div>
+<div class="card-body">
+    <?php
+        $authorName = $_POST['authorName'];
+
+        try {
+          if (!$authorName) {
+            throw new Exception('Author details not complete. Please try again.');
+          }
+
+          @ $db = new mysqli('127.0.0.1:3306', 'student', '123qwe', 'php_lesson_db');
+
+          $dbError = mysqli_connect_errno();
+          if ($dbError) {
+            throw new Exception('Error: Could not connect to database. Please try again.');
+          }
+
+          // QUERY BY QUERY STRING CONCATENATION
+          // $query = 'insert into author (name) values (\''.$authorName.'\')';
+          // $result = $db->query($query);
+          //
+          //
+          // if ($result) {
+          //   echo $db->affected_rows." author inserted into the database.";
+          // }else {
+          //   throw new Exception("Error: Author was not added.");
+          // }
+
+          // QUERY BY PREPARED STATEMENTS
+          $query = 'insert into author (name) values (?)';
+          $stmt = $db->prepare($query);
+          $stmt->bind_param("s", $authorName);
+
+          $stmt->execute();
+
+          @ $affectedRows = $stmt->$affected_rows;
+
+          if ($affectedRows > 0) {
+            echo $affectedRows." author inserted into the database.";
+          }else {
+            throw new Exception("Error: Author was not added.");
+          }
+
+          $stmt->close();
+
+        } catch (Exception $e) {
+          echo $e->getMessage();
+        }
+
+     ?>
+</div>
+<div class="card-footer">
+  <a class="btn btn-dark" href="author-add.php">Back</a>
+</div>
+
+<?php require_once('view-comp/footer.php'); ?>
