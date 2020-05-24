dcd6d8478ec6fd1f1b898590312369ccfb147b0a
paolotormon <mdawntormon@gmail.com>
2020-05-25 00:52:00 +0800

    h6: Replaced real_escape_string with prepared statements. Also added check for duplicate book

--- book-catalog/book-add.php
+++ book-catalog/book-add.php
@@ -10,10 +10,10 @@ if (isset($_POST['submit'])) {
             <b>Add Book Result</b>
          </div>';
 
-   //not inside try catch because if we make this throw an 
-   //exception, an uncaught error will be thrown at the finally
-   //block when closing mysqli (since $db would be null and 
-   //we can't close a null connection).
+   //The condition below is 'if else' instead of 'try catch' because  
+   //if we make it 'try catch', an uncaught error will be thrown at
+   //the finally block when closing mysqli. This is because the mysqli 
+   //object is null and we can't close a null connection).
    if (empty($bookTitle) || empty($authorName)) {
 
       echo '<div class="m-4"> 
@@ -29,36 +29,49 @@ if (isset($_POST['submit'])) {
             Please try again later.');
          }
 
-         //Clean Strings
-         $authorName = $db->real_escape_string($authorName);
-         $bookTitle = $db->real_escape_string($bookTitle);
+         
+         // $authorName = $db->real_escape_string($authorName);
+         // $bookTitle = $db->real_escape_string($bookTitle);
+         // These clean Strings are not optimal since titles of
+         // books with legit apostrophes (ex. Can't Hurt Me)
+         // get corrupted when uploaded to database
 
-         //PRO: Query below merges checking and inserting for 1 trip to db instead of 2 
-         //CON: $result is always true
-         $query =
-            'INSERT INTO author (name)
-            SELECT * FROM (SELECT \'' . $authorName . '\') AS temp
-            WHERE NOT EXISTS(
-              SELECT name FROM author WHERE name = \'' . $authorName . '\'
-            ) LIMIT 1';
-         $result = $db->query($query);
-
-         // $query =
-         //    'INSERT INTO book (title, author_id) values (\'' . $bookTitle . '\',
-         //    (SELECT id FROM author WHERE name = \'' . $authorName . '\'))';
-
-         // $result = $db->query($query);
-
-         $query = 'INSERT INTO book (title, author_id) values (?, (SELECT id FROM author WHERE name = \'' . $authorName . '\'))';
+         //Query below merges checking and inserting of author 
+         //for 1 trip to db instead of 2 
+         $query = 
+         'INSERT INTO author (name)
+         SELECT * FROM (SELECT ?) AS temp
+         WHERE NOT EXISTS
+         (SELECT name FROM author WHERE name = ?
+            )LIMIT 1';
          $stmt = $db->prepare($query);
-         $stmt->bind_param("s",$bookTitle);
+         $stmt->bind_param("ss",$authorName,$authorName);
+         $stmt->execute();
+
+         //Checks for duplicate books before inserting 
+         //for 1 trip to db instead of 2.
+         //Note: we used book title and author name (instead of ISBN)
+         //as the determinators of duplicate books 
+         //for easier testing. Need to put ? AS x to make books with
+         //same name and author (however unlikely) be possible.
+         $query = 
+         'INSERT INTO book (title, author_id) 
+         SELECT * FROM (SELECT ? AS title, (SELECT id FROM author WHERE name = ?) AS author_id) AS temp
+         WHERE NOT EXISTS
+         (SELECT * FROM book WHERE title = ? AND author_id = (SELECT id FROM author WHERE name = ?)
+         )LIMIT 1';
+      
+         $stmt = $db->prepare($query);
+         $stmt->bind_param("ssss",$bookTitle,$authorName,$bookTitle,$authorName);
          $stmt->execute();
          $affectedRows=$stmt->affected_rows;
+         echo 'affected rows: '.$affectedRows;
          $stmt->close();
          
          if ($affectedRows>0) {
             echo '<div class="m-4">Book successfully added to Catalog!</div>';
          } else {
+            //note:'ALTER TABLE book ADD CONSTRAINT un_book_specs UNIQUE (title,author_id)'; was executed at the start
             throw new Exception('Error: The book you entered, <b>' . $bookTitle . ' by ' . $authorName . '</b> is already in the database!');
          }
       } catch (Exception $e) {
