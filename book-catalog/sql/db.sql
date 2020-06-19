522c8faf274dae5233892cb4dd154f618406fb6e
unknown <201801013@iacademy.edu.ph>
2020-06-19 14:38:51 +0800

    added db sql

---
+++ book-catalog/sql/db.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE php_lesson_db;
+
+USE php_lesson_db;
+
+CREATE TABLE IF NOT EXISTS author(
+  ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  NAME varchar (255)
+);
+
+CREATE TABLE IF NOT EXISTS book(
+  ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  TITLE VARCHAR (255),
+  ISBN VARCHAR(255),
+  AUTHOR_ID INT(6) UNSIGNED,
+  FOREIGN KEY (AUTHOR_ID) REFERENCES AUTHOR(ID)
+);
+
+INSERT INTO author (NAME)
+VALUES
+ ('Michael Morgan'),
+ ('George RR Martin');
+
+ INSERT INTO book ( TITLE, ISBN, AUTHOR_ID)
+ VALUES
+  ('Java for Professional Developers', '0-672-316123-8',1),
+  ('A Game of Thrones', '1-141-5143123-5',2);
