39a1e6bd4dc3f77ed0013d21a506ea84f1cd7f20
datuemirjaafar <61898168+datuemirjaafar@users.noreply.github.com>
2020-07-14 13:50:20 +0800

    Create add-book-assignments-5-6.php

---
+++ add-book-assignments-5-6.php
@@ -0,0 +1,17 @@
+<?php require_once('view-comp/header.php'); ?>
+<div class="card-header">
+  Add Book Result
+</div>
+<div class="card-body">
+  <div class="" style="text-align: center;">
+    <h2>Failed to add book!</h2>
+  </div>
+
+  <form action="index.php" method="post">
+
+    <div>
+      <button type="submit" class="btn btn-primary">Back to Home</button>
+    </div>
+  </form>
+</div>
+<?php require_once('view-comp/footer.php'); ?>
