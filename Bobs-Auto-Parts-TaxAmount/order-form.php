a2ff10c1979942c060b8d4f2dcac431ace0c3b7c
zone301 <enzo.stjohn@gmail.com>
2020-07-14 10:41:21 +0800

    edited act3

--- Bobs-Auto-Parts-TaxAmount/order-form.php
+++ Bobs-Auto-Parts-TaxAmount/order-form.php
@@ -9,7 +9,6 @@
               <thead>
                 <tr class="row">
                   <th class="col-4">Item</th>
-                  <th class="col-5">Price</th>
                   <th class="col-3">Quantity</th>
                 </tr>
               </thead>
@@ -19,9 +18,6 @@
                     echo '<tr class="row">
                             <td class="col-5">
                               '.$productInput->desc.'
-                            </td>
-                            <td class="col-2">
-                              '.$productInput->price.'
                             </td>
                             <td class="col-4">
                               <input type="text" name="'.$productInput->name.'" maxlength="3" class="form-control"/>
@@ -39,7 +35,7 @@
               </tbody>
             </table>
           </form>
-          
+
           <?php
             require_once('view/footer.php');
           ?>
