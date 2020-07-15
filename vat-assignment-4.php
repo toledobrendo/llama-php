3e6e40dc4832a171d90315829e586e7b9a45d215
datuemirjaafar <61898168+datuemirjaafar@users.noreply.github.com>
2020-07-15 10:57:15 +0800

    Create vat-assignment-4.php

---
+++ vat-assignment-4.php
@@ -0,0 +1,32 @@
+<?php
+
+	function getVatRate() {
+
+		$file = @ fopen(DOCUMENT_ROOT.'/llama-php/assignments/vat-percent/resource/properties.txt', 'rb');
+		$value_arr = array();
+		$counter = 0;
+
+		if(!$file) {
+			echo '<strong>VAT Amount was not shown. Please try again later.</strong>';
+		} else{
+
+			while(!feof($file)) {
+
+				$var = fgetc($file);
+
+				if(!ctype_alpha($var) && $var != '_'  && $var != '=' && $var != ' ') {
+					$value_arr[$counter] = $var;
+					echo $var;
+					$counter++;
+				}
+
+			}
+
+		}
+
+		fclose($file);
+		return implode($value_arr);
+	}
+
+
+ ?>
